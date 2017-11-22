b30d689efed1eedce8a6369e4cce711b5153d691
Phillip Lai <phillyp.lai@gmail.com>
2017-11-21 16:58:28 -0800

    added kmeans functionality

--- Object_Detection/cpp_branch/roi_keypoint.cpp
+++ Object_Detection/cpp_branch/roi_keypoint.cpp
@@ -18,8 +18,15 @@ int main( int argc, char** argv )
     //padding color
     cv::Scalar padding_color = cvScalar(255,255,255);
 
+    //kNN parameters
+    int k = 3;
+    cv::Mat labels;
+    int attempts = 30;
+    cv::Mat centers;
+
     if( argc != 2 ){
-        readme(); return -1;
+        readme();
+        return -1;
     }
 
     cv::Mat img = imread (argv[1]);
@@ -53,15 +60,40 @@ int main( int argc, char** argv )
         y = (i->pt.y)-(roi_width/2);
         std::cout << "x: " << x << " y: " << y << std::endl;
 
-        //name for each window
+        //name for each window for ROI
         sprintf(window_name, "ROIno.%d", counter);
         //if the ROI region that we are cropping out goes out of bounds, adds a boder to it as padding
         cv::Mat roi_image = getPaddedROI(img, x, y, roi_width, roi_width, padding_color);
-        imshow(window_name, roi_image);
+        //imshow(window_name, roi_image);
 
-    }
+        //hsv conversion before using kNN
+        sprintf(window_name, "HSVno.%d", counter);
+        //cvtColor(roi_image, roi_image, CV_BGR2HSV);
+        //imshow(window_name, roi_image);
 
-    //imshow("img_hsv", img_hsv);
+        //kNN
+        sprintf(window_name, "kNNno.%d", counter);
+        cv::Mat samples(roi_image.rows * roi_image.cols, 3, CV_32F);
+            for( int y = 0; y < roi_image.rows; y++ )
+                for( int x = 0; x < roi_image.cols; x++ )
+                    for( int z = 0; z < 3; z++)
+                        samples.at<float>(y + x*roi_image.rows, z) = roi_image.at<Vec3b>(y,x)[z];
+
+        kmeans(samples, k, labels, TermCriteria(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers );
+
+        Mat kmeans_image( roi_image.size(), roi_image.type() );
+        for( int y = 0; y < roi_image.rows; y++ )
+            for( int x = 0; x < roi_image.cols; x++ )
+            {
+                int cluster_idx = labels.at<int>(y + x*roi_image.rows,0);
+                kmeans_image.at<Vec3b>(y,x)[0] = centers.at<float>(cluster_idx, 0);
+                kmeans_image.at<Vec3b>(y,x)[1] = centers.at<float>(cluster_idx, 1);
+                kmeans_image.at<Vec3b>(y,x)[2] = centers.at<float>(cluster_idx, 2);
+            }
+            //cvtColor(new_image, new_image, CV_HSV2BGR);
+            imshow( window_name, kmeans_image );
+            //std::cout << centers << std::endl;
+        }
 
     cv::waitKey(0);
     return 0;
